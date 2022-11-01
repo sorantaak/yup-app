0bf5253d4d2dce729357e8aaecb8ec23d8abd432
soran <soran.taak@gmail.com>
2022-11-01 12:00:19 +0330

    install yup library

--- src/App.js
+++ src/App.js
@@ -1,25 +1,40 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
+import { object, string } from "yup";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+	const validateData = async () => {
+		let loginFormSchema = object({
+			email: string().required(),
+			password: string().required(),
+		});
+		try {
+			await loginFormSchema.validate({}, { abortEarly: false });
+		} catch (e) {
+			console.log(e, e.errors);
+		}
+	};
+
+	validateData();
+
+	return (
+		<div className="App">
+			<header className="App-header">
+				<img src={logo} className="App-logo" alt="logo" />
+				<p>
+					Edit <code>src/App.js</code> and save to reload.
+				</p>
+				<a
+					className="App-link"
+					href="https://reactjs.org"
+					target="_blank"
+					rel="noopener noreferrer"
+				>
+					Learn React
+				</a>
+			</header>
+		</div>
+	);
 }
 
 export default App;

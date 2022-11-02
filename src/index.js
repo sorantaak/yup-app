bfd57bf6c3b52e384f5f6cfc1083de52c9f89cc8
soran <soran.taak@gmail.com>
2022-11-02 13:29:12 +0330

    set custom error based on locale language

--- src/index.js
+++ src/index.js
@@ -3,8 +3,19 @@ import ReactDOM from "react-dom/client";
 import "./index.css";
 import App from "./App";
 import reportWebVitals from "./reportWebVitals";
+import { setLocale } from "yup";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
+
+setLocale({
+	mixed: {
+		required: ({ path }) => `فیلد ${path} الزامی است`,
+	},
+	string: {
+		min: ({ path, min }) => `حداقل طول فیلد ${path} ${min} کاراکتر می باشد`,
+		email: ({ path }) => `ایمیل صحیح نیست`,
+	},
+});
 root.render(<App />);
 
 // If you want to start measuring performance in your app, pass a function
